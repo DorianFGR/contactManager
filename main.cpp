fca3715683006f3b9439cf4c9d9ce4d18f28c8f5
Dorian Figueiras <dorianfigueiras@icloud.com>
2024-12-16 10:42:02 +0100

    viewAllContacts function added

--- main.cpp
+++ main.cpp
@@ -33,6 +33,32 @@ void addContact(std::vector<contact>& contacts){
 
 }
 
+void viewAllContacts(std::vector<contact>& contacts) {
+
+
+    int lenght = contacts.size();
+    std::cout << lenght << std::endl;
+
+    for (int i = 0; i < lenght; i++) {
+
+        std::cout<<"Contact number : " + i ;
+
+        std::cout<<"\n_____________________\n";
+        std::cout<<"NickName : \n";
+        std::cout << contacts[i].nickname << std::endl;
+        std::cout<<"Name : \n";
+        std::cout << contacts[i].name << std::endl;
+        std::cout<<"phone Number : \n";
+        std::cout << contacts[i].phoneNumber << std::endl;
+        std::cout<<"Email : \n";
+        std::cout << contacts[i].email << std::endl;
+        std::cout<<"Address : \n";
+        std::cout << contacts[i].address << std::endl;
+        std::cout<<"_____________________\n";
+
+    }
+}
+
 
 int main() {
 
@@ -49,6 +75,9 @@ int main() {
         std::cout<<"4. View all contacts\n";
         std::cout<<"5. Exit\n";
 
+
+        std::cout<<"_____________________\n";
+        std::cout<<"\nEnter your choice: ";
         std::cin >> choice;
 
 
@@ -57,6 +86,11 @@ int main() {
             case 1:
 
                 addContact(contacts);
+                break;
+
+            case 2:
+
+                viewAllContacts(contacts);
                 break;
 
             case 5:
