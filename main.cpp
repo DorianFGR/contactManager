1757fa100b7316bef6efd7a40d89151d1c58f562
Dorian Figueiras <dorianfigueiras@icloud.com>
2024-12-17 22:18:05 +0100

    save contacts function finished.

--- main.cpp
+++ main.cpp
@@ -3,9 +3,10 @@
 #include <limits>
 #include <vector>
 #include <string>
+#include <filesystem>
 #include "include/json.hpp"
 
-using json =nlohmann::json;
+using json = nlohmann::json;
 
 struct contact {
     std::string nickname;
@@ -31,50 +32,64 @@ void from_json(const nlohmann::json &j, contact &c) {
 
 }
 
-bool save_contact_to_file(const contact &c, const std::string &filename) {
+
+bool save_contacts_to_file(const std::vector<contact>& contacts, const std::string &filename) {
 
     std::ofstream file(filename);
+
     if (!file.is_open()) {
 
-        std::cerr << "Error : Could not open file " << filename << std::endl;
+        std::cerr << "Error: Could not open file " << filename << std::endl;
         return false;
+
     }
 
-    json j = c;
+    json j = contacts;
     file << j.dump(4);
     file.close();
     return true;
+
 }
 
-bool load_contact_from_file(contact &c, const std::string &filename) {
+bool load_contacts_from_file(std::vector<contact>& contacts, const std::string &filename) {
 
     std::ifstream file(filename);
+
     if (!file.is_open()) {
 
-        std::cerr << "Error : Could not open file " << filename << std::endl;
+        std::cerr << "Error: Could not open file " << filename << std::endl;
+        std::cerr << "Current path is " << std::filesystem::current_path() << std::endl;
         return false;
+
+    }
+
+
+    if (file.peek() == std::ifstream::traits_type::eof()) {
+
+        return true;
+
     }
 
     json j;
-    file >> j;
 
     try {
 
-        c = j.get<contact>();
+        file >> j;
+        contacts = j.get<std::vector<contact>>();
 
-    }catch (json::exception &e) {
+    } catch (json::exception &e) {
 
-        std::cerr << "Error during JSON convertion " << e.what() << std::endl;
+        std::cerr << "Error during JSON conversion: " << e.what() << std::endl;
         return false;
 
     }
 
     return true;
-
 }
 
 
 void addContact(std::vector<contact>& contacts) {
+
     contact newContact;
 
     std::cout << "Enter Nickname: ";
@@ -97,58 +112,46 @@ void addContact(std::vector<contact>& contacts) {
     contacts.push_back(newContact);
     std::cout << "Contact Added" << std::endl;
 
-
 }
 
-void viewAllContacts(std::vector<contact>& contacts) {
+void viewAllContacts(const std::vector<contact>& contacts) {
 
+    for (size_t i = 0; i < contacts.size(); ++i) {
 
-    int lenght = contacts.size();
-
-    for (int i = 0; i < lenght; i++) {
-
-        std::cout<<"Contact number : ";
-        std::cout<<i +1;
-
-        std::cout<<"\n_____________________\n";
-        std::cout<<"NickName : ";
-        std::cout << contacts[i].nickname << std::endl;
-        std::cout<<"Name : ";
-        std::cout << contacts[i].name << std::endl;
-        std::cout<<"phone Number : ";
-        std::cout << contacts[i].phoneNumber << std::endl;
-        std::cout<<"Email : ";
-        std::cout << contacts[i].email << std::endl;
-        std::cout<<"Address : ";
-        std::cout << contacts[i].address << std::endl;
-        std::cout<<"_____________________\n";
+        std::cout << "Contact number: " << i + 1 << "\n";
+        std::cout << "_____________________\n";
+        std::cout << "Nickname: " << contacts[i].nickname << "\n";
+        std::cout << "Name: " << contacts[i].name << "\n";
+        std::cout << "Phone Number: " << contacts[i].phoneNumber << "\n";
+        std::cout << "Email: " << contacts[i].email << "\n";
+        std::cout << "Address: " << contacts[i].address << "\n";
+        std::cout << "_____________________\n";
 
     }
 }
 
-void viewSpecificContacts(std::vector<contact>& contacts) {
+void viewSpecificContacts(const std::vector<contact>& contacts) {
 
     std::string contactNumber;
 
-    std::cout<<"Contact number : ";
+    std::cout << "Contact number: ";
     std::cin >> contactNumber;
 
     try {
 
-        int contactIndex = std::stoi(contactNumber);
-
+        int contactIndex = std::stoi(contactNumber) - 1;
 
         if (contactIndex < 0 || contactIndex >= contacts.size()) {
+
             std::cout << "Invalid contact index!" << std::endl;
             return;
+
         }
 
-        contactIndex -= 1;
         const contact& contact = contacts[contactIndex];
 
-
         std::cout << "\n_____________________\n";
-        std::cout << "NickName: " << contact.nickname << std::endl;
+        std::cout << "Nickname: " << contact.nickname << std::endl;
         std::cout << "Name: " << contact.name << std::endl;
         std::cout << "Phone Number: " << contact.phoneNumber << std::endl;
         std::cout << "Email: " << contact.email << std::endl;
@@ -164,45 +167,44 @@ void viewSpecificContacts(std::vector<contact>& contacts) {
         std::cout << "Contact number is out of range!" << std::endl;
 
     }
-
 }
 
 void deleteContact(std::vector<contact>& contacts) {
 
     std::string contactNumber;
 
-    std::cout<<"Contact number : ";
+    std::cout << "Contact number: ";
     std::cin >> contactNumber;
 
     try {
 
-        int contactIndex = std::stoi(contactNumber);
-
-        contactIndex -= 1;
+        int contactIndex = std::stoi(contactNumber) - 1;
 
         if (contactIndex < 0 || contactIndex >= contacts.size()) {
+
             std::cout << "Invalid contact index!" << std::endl;
             return;
-        }
 
+        }
 
         const contact& contactToDelete = contacts[contactIndex];
 
-
         std::cout << "Are you sure you want to delete contact: " << contactToDelete.name
                   << " (Phone: " << contactToDelete.phoneNumber << ")? (y/n): ";
+
         char confirmation;
         std::cin >> confirmation;
 
-
         if (confirmation == 'y' || confirmation == 'Y') {
+
             contacts.erase(contacts.begin() + contactIndex);
             std::cout << "Contact deleted successfully." << std::endl;
+
         } else {
+
             std::cout << "Contact deletion canceled." << std::endl;
+
         }
-
-
     } catch (const std::invalid_argument& e) {
 
         std::cout << "Invalid contact number!" << std::endl;
@@ -212,75 +214,79 @@ void deleteContact(std::vector<contact>& contacts) {
         std::cout << "Contact number is out of range!" << std::endl;
 
     }
-
 }
-
 
 int main() {
 
     std::vector<contact> contacts;
     int choice;
 
-    do{
 
-        std::cout<<"\nContact Manager\n";
-        std::cout<<"_____________________\n";
-        std::cout<<"1. Add a new contact\n";
-        std::cout<<"2. View all contacts\n";
-        std::cout<<"3. View a specific contact\n";
-        std::cout<<"4. Delete a contact\n";
-        std::cout<<"5. Exit\n";
+    std::string filename = std::filesystem::current_path().string() + "/saves/contacts.json";
 
+    if (load_contacts_from_file(contacts, filename)) {
 
-        std::cout<<"_____________________\n";
-        std::cout<<"\nEnter your choice: ";
+        std::cout << "Contacts loaded from file." << std::endl;
+
+    } else {
+
+        std::cerr << "Failed to read from file!" << std::endl;
+
+    }
+
+    do {
+
+        std::cout << "\nContact Manager\n";
+        std::cout << "_____________________\n";
+        std::cout << "1. Add a new contact\n";
+        std::cout << "2. View all contacts\n";
+        std::cout << "3. View a specific contact\n";
+        std::cout << "4. Delete a contact\n";
+        std::cout << "5. Exit\n";
+        std::cout << "_____________________\n";
+        std::cout << "\nEnter your choice: ";
         std::cin >> choice;
 
-
-        switch(choice) {
+        switch (choice) {
 
             case 1:
-
                 addContact(contacts);
 
-                if (save_contact_to_file(contact, "contact.json")) {
-                    std::cout << "Le fichier a été sauvegardé avec succès !" << std::endl;
-                }
+                if (save_contacts_to_file(contacts, filename)) {
 
+                    std::cout << "Contacts saved successfully!" << std::endl;
+
+                }
                 break;
 
             case 2:
-
                 viewAllContacts(contacts);
                 break;
 
             case 3:
-
                 viewSpecificContacts(contacts);
                 break;
 
             case 4:
-
                 deleteContact(contacts);
+
+                if (save_contacts_to_file(contacts, filename)) {
+
+                    std::cout << "Contacts saved successfully!" << std::endl;
+
+                }
                 break;
 
-
             case 5:
-
-                std::cout<<"\n Exiting...";
+                std::cout << "\nExiting...";
                 break;
 
             default:
-
-                std::cout<<"\n Invalid Choice\n Please try again";
+                std::cout << "\nInvalid choice. Please try again.";
 
         }
 
-    }while(choice !=5);
+    } while (choice != 5);
 
     return 0;
 }
-
-
-
-
